90686cfbe2a49df032455430f5138f296f1e97cf
RarufuSama <rarufutomasu@gmail.com>
2014-08-18 15:55:20 +0800

    My First attempt

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,51 +1,49 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.http import Http404
-from django.shortcuts import render 
 from django.shortcuts import render, get_object_or_404
+from polls.models import Choice, Poll
+from django.views import generic
+from django.core.urlresolvers import reverse
+from django.utils import timezone
 
 
 
 from polls.models import Poll
 
-def index (request):
-	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-	context = {'latest_poll_list' : latest_poll_list}
-	return render(request, 'polls/index.html', context)
 
-def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(template.render(context))
 
-def index(request):
-	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-	output = ', '.join([p.question for p in latest_poll_list]) 
-	return HttpResponse(output)
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_poll_list'
 
-def index(request):
-	return HttpResponse("Hello, world. You're at the poll index")
+	def get_queryset(self):
+		return Poll.objects.filter(
+			pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
-def detail(request, poll_id):
-	return HttpResponse("You're looking at poll %s." % poll_id)
+class DetailView(generic.DetailView):
+	model = Poll
+	template_name = 'polls/detail.html'
 
-def results(request, poll_id):
-	return HttpResponse("You're looking at the results of poll %s." %poll_id)
+	def get_queryset(self, *args, **kwargs):
+		return Poll.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+	model = Poll
+	template_name = 'polls/results.html'		
 
 def vote(request, poll_id):
-	return HttpResponse("You're voting on poll %s." % poll_id)
-
-def detail(request, poll_id):
+	p = get_object_or_404(Poll, pk=poll_id)
 	try:
-		poll = Poll.objects.get(pk=poll_id)
-	except Poll.DoesNotExist:
-		raise Http404
-	return render(request, 'polls/detail.html', {'poll':poll})		
+		selected_choice = p.choice_set.get(pk=request.POST['choice']) 
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'poll': p,
+			'error_message': "You didn't select a choice."})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()	
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
-def detail(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/detail.html', {'poll': poll})	
+class HomeView(generic.TemplateView):
+	template_name = 'polls/home.html'
